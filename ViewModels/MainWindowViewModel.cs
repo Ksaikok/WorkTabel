aa912d244f7f34bbdb6dacc7573754ae9a40e5b1
Семён <@DESKTOP-UUP0TGU>
2024-05-07 00:32:58 +0700

    Написал вьюмодел и мейнвиндоввьюмодел

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkTabel.ViewModels.Base;
+
+namespace WorkTabel.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title;
+
+        public string Title 
+        {
+            set => Set(ref _Title, value);
+        }
+
+    }
+}
