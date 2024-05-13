1a7577a0103c978cae6970c40e06482a6195286a
Семён <@DESKTOP-UUP0TGU>
2024-05-13 21:17:22 +0700

    В ЛямбдаКоммандс написали код для закрытия приложения по кнопке

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,46 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WorkTabel.Infrastructure.Commands;
 using WorkTabel.ViewModels.Base;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WorkTabel.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        //private string _Title = "Главная страница";
+        #region Команды
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        #endregion
+        #endregion
 
-        //public string Title 
+        public MainWindowViewModel()
+        {
+            #region Команды
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
+        //internal class MainWindowViewModel : ViewModel
         //{
-        //    get => _Title;
-        //    set => Set(ref _Title, value);
-        //}
+        //    //private string _Title = "Главная страница";
 
+        //    //public string Title 
+        //    //{
+        //    //    get => _Title;
+        //    //    set => Set(ref _Title, value);
+        //    //}
+
+        //}
     }
 }
