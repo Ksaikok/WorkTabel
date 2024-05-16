a92e4ee7e615d4cd048359e6e44776e0cf066210
Семён <@DESKTOP-UUP0TGU>
2024-05-16 19:48:40 +0700

    Настроена главная страница припомощи DockPanel

---
+++ Model/ObIrtish/Sotrudnik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTabel.Model.ObIrtish
+{
+    internal class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FullName { get; set; }
+        public DateTime EnterTime { get; set; }
+        public DateTime ExitTime { get; set; }
+        public int WorkedOut {  get; set; }
+        public string AttendanceType { get; set; } = "П";
+    }
+    internal class Department
+    {
+        public int DepartmentID {  get; set; }
+        public string DepartmentName { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+    }
+
+}
