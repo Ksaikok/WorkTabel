0cd96c9a132030b10fd893cb5c3684df3cd70f35
Семён <@DESKTOP-UUP0TGU>
2024-05-17 20:57:31 +0700

    Добавлены Кнопки, DatePicker и вкладка "Сотрудники"

--- Model/ObIrtish/Sotrudnik.cs
+++ Model/ObIrtish/Sotrudnik.cs
@@ -12,12 +12,12 @@ namespace WorkTabel.Model.ObIrtish
         public string FullName { get; set; }
         public DateTime EnterTime { get; set; }
         public DateTime ExitTime { get; set; }
-        public int WorkedOut {  get; set; }
+        public int WorkedOut { get; set; }
         public string AttendanceType { get; set; } = "П";
     }
     internal class Department
     {
-        public int DepartmentID {  get; set; }
+        public int DepartmentID { get; set; }
         public string DepartmentName { get; set; }
         public ICollection<Employee> Employees { get; set; }
     }
